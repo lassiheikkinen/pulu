a410721c4bc81ccdd0246fbb9d17372a67c47ba1
Lassi Heikkinen <lassi@pulu.org>
2015-11-29 10:04:18 +0000

    original files created in 2005 or something

---
+++ index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+ <title>pulu.org</title>
+ 
+ <style type="text/css">
+
+a:link	         {text-decoration: underline;}
+a:visited	 {text-decoration: underline;}
+a:active	 {text-decoration: underline;}
+a:hover	         {text-decoration: none;}
+p                {font-size: 9px; font-family: verdana; margin-top: 10px;} 
+
+ </style>
+ <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+</head>
+
+<body style="margin: 0 20%; background-color: #5d89b5">
+
+<div><p style="font-size: 16px; margin-bottom: 15px; letter-spacing: 10px"><b>pulu.org</b></p></div>
+
+<div><img src="real_pulu.jpg" alt="real pulu.org" style="border: 2px solid #000" /></div>
+
+
+<div style="margin-left: 50px; margin-top: 15px; padding: 3px; border: 2px solid #000; background-color: #FFF; width: 240px; color: #000;">
+Sites under pulu.org:<br /><br />
+<a style="color: #000" href="/lassi">Lassite</a> - personal page<br />
+<a style="color: #000" href="http://palsta.pulu.org">Pulupalsta</a> - miscellaneous articles<br />
+<a style="color: #000" href="http://www.moposite.com">Moposite</a> - Elasto Mania all around<br />
+</div>
+
+
+<div style="width: 604px;">
+<br />
+  <img src="info.png" style="float: left;" alt="" />
+
+  <div style="margin-left: 50px; padding: 3px; text-align: justify; border: 2px solid #000; background-color: #d6da64">
+
+<p style="margin: 0px;">
+pulu.org domain was created and registered in the 13th February 2002 by Lassi Heikkinen aka Abula. Pulu is Finnish and means a pigeon (the bird in the picture).
+</p>
+
+<p style="margin-bottom: 0px;">
+The above photograph is taken by mr.
+</p>
+
+  </div>
+
+
+</div>
+
+
+<!--
+<div style="width: 200px;">
+<br />
+  <img src="links.png" style="float: left;" alt="" />
+
+  <div style="margin-left: 50px; padding: 3px; border: 2px solid #000; background-color: #bfa685">
+
+
+<p style="margin: 0px;">
+- <a title="Rather big site decicated for a game named Elasto Mania." href="http://www.moposite.com">Moposite</a><br />
+- <a title="Projects by Lassi Heikkinen" href="http://www.pulu.org/pro">Puluprojects</a><br />
+- <a title="Blog of Elma and personal life." href="http://www.pulu.org/blog">Applelane</a><br />
+- <a title="Lassi's personal homepage" href="http://www.pulu.org/lassi">Lassite</a><br />
+- <a title="A rock band page whose members are friends of Lassi." href="http://www.luminapolaris.net/">Lumina Polaris</a><br />
+- <a title="A studies page of gynecology and obstetrics of Oulu university." href="http://www.oulugyn.net/">Naisen elämänkaari</a><br />
+<br />
+- <a title="A collection of old web pages made by me, not all-inclusive." href="http://www.pulu.org/vanha">Old pages</a>
+</p>
+
+
+<p style="margin-bottom: 0px;">
+<b>Progressing</b><br />
+- <a title="A picture gallery." href="http://www.otit.fi/~krabula/gallery">Pulukuvasto</a><br />
+- <a title="Lassi's personal homepage" href="http://www.pulu.org/lassi">Lassite3</a><br />
+</p>
+
+<p style="margin-bottom: 0px;">
+<b>Retired</b><br />
+- <a title="An interesting hobby to collect pictures of all the Finnish municipality signs." href="http://www.pulu.org/kuntakulma">Kuntakulma</a><br />
+- <a title="An older version of the personal homepage of Lassi. Yet, it's not half done." href="http://www.pulu.org/lassite">Lassite</a><br />
+- <a title="Lassi's personal homepage; has more information about the web sites." href="http://www.pulu.org/lassite2">Lassite2</a><br />
+- <a title="An attempt to save the world." href="http://www.pulu.org/tupakka">Tupakka ympärillämme</a><br />
+- <a title="An inside page of the friends of our gang, in the past." href="http://www.pulu.org/seppolaakso">Seppolaakso</a><br />
+</p>
+-->
+  </div>
+
+</div>
+
+<div style="margin-top: 10px; width: 604px; text-align: right;"><img src="logo_060809.gif" alt="pulu.org logo" /></div>
+
+</body>
+</html>

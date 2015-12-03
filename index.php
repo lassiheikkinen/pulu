5103b1e3f2fa79570b4b369dca99b021ec220b66
Lassi Heikkinen <lassi@pulu.org>
2015-12-03 16:56:21 +0000

    change: sites listed in running age order

--- index.php
+++ index.php
@@ -70,14 +70,14 @@ a:hover	    {text-decoration: none;}
 
 <h2>Active sites under <strong class="highlighter">pulu.org</strong></h2>
 <ul>
-    <li><a href="http://pulu.org/blog/" title="Lassi's personal blog">Applelane</a> <span class="since" title="The site has been running since">since 2005</span></li>
-    <li><a href="http://pulu.org/lassi/" title="Lassi's personal site">Lassite</a> <span class="since" title="The site has been running since">2000</span></li>
-    <li><a href="http://liikuntoilu.net/" title="API to historical sport exercicing data">Liikuntoilu</a> <span class="since" title="The site has been running since">2006</span></li>
+    <li><a href="http://pulu.org/lassi/" title="Lassi's personal site">Lassite</a> <span class="since" title="The site has been running since">since 2000</span></li>
     <li><a href="http://www.moposite.com/" title="A site dedicated to Elasto Mania game">Moposite</a> <span class="since" title="The site has been running since">2000</span></li>
-    <li><a href="http://unski.pulu.org/onki/" title="Johanna's fishing blog">Onki</a> <span class="since" title="The site has been running since">2009</span></li>
+    <li><a href="http://pulu.org/blog/" title="Lassi's personal blog">Applelane</a> <span class="since" title="The site has been running since">2005</span></li>
     <li><a href="http://palsta.pulu.org/" title="Lassi's projects and articles">Pulupalsta</a> <span class="since" title="The site has been running since">2006</span></li>
-    <li><a href="http://rohtologi.pulu.org/" title="Johanna's pharmaceutical blog">Rohtologi</a> <span class="since" title="The site has been running since">2010</span></li>
+    <li><a href="http://liikuntoilu.net/" title="API to historical sport exercicing data">Liikuntoilu</a> <span class="since" title="The site has been running since">2006</span></li>
     <li><a href="http://ulko-ovimiehet.liikuntoilu.net/" title="Outdoorsmen's sport competition">Ulko-ovimiehet</a> <span class="since" title="The site has been running since">2007</span></li>
+    <li><a href="http://unski.pulu.org/onki/" title="Johanna's fishing blog">Onki</a> <span class="since" title="The site has been running since">2009</span></li>
+    <li><a href="http://rohtologi.pulu.org/" title="Johanna's pharmaceutical blog">Rohtologi</a> <span class="since" title="The site has been running since">2010</span></li>
 </ul>
 
 <p class="footnote">Ps. <em>pulu</em> is a Finnish word and it means <a href="https://en.wikipedia.org/wiki/Columbidae">pigeon</a>, the bird in the logo.</p>
